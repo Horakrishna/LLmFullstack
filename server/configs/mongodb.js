9fa4da9b6cc37db0240a659650b2f626d6634077
Pallab Hk <horakrishnaroy@gmail.com>
2025-02-22 14:21:49 +0600

    Create user data

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,6 +4,6 @@ import mongoose from "mongoose"
 const connectDB = async()=>{
     mongoose.connection.on('connected',()=>console.log('Database Connected'))
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/LLM`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 }
 export default connectDB
